67cd70fcfb4f3cfe531d99ee66dca7cfbb58c9ed
Simon Greve <yolomastersimon@gmail.com>
2022-12-22 12:52:32 +0100

    complete Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,36 @@
 fun main() {
+
+    /**
+     * Splits a list divided by empty lines into sub lists
+     */
+    fun findSubLists(input: List<String>): List<List<String>> {
+        val subLists: MutableList<List<String>> = mutableListOf()
+        var segmentStart: Int
+        var segmentEnd = 0
+        for (index in input.indices) {
+            if (input[index] == "") {
+                segmentStart = segmentEnd
+                segmentEnd = index
+                subLists.add(input.subList(segmentStart, segmentEnd).filter { it != "" })
+            }
+        }
+        return subLists
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val subLists = findSubLists(input)
+        return subLists.maxOf { list -> list.sumOf { it.toInt() } }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val subLists = findSubLists(input)
+        val calories = mutableListOf<Int>()
+        for (list in subLists) {
+            calories.add(list.sumOf { it.toInt() })
+        }
+        calories.sortDescending()
+        return calories[0] + calories[1] + calories[2]
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
